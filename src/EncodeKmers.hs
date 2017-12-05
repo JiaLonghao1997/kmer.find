82685749c87f133c264f3187aeee93383ceb860e
Luis Pedro Coelho <luis@luispedro.org>
2017-12-06 00:14:09 +0100

    RFCT Split off kmer encoding to separate file

--- src/EncodeKmers.hs
+++ src/EncodeKmers.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE LambdaCase, OverloadedStrings, FlexibleContexts, BangPatterns, PackageImports #-}
 
-import qualified Data.ByteString as B
 import qualified Data.Conduit as C
 import           Data.Conduit ((.|))
 import qualified Data.Conduit.Combinators as CC
@@ -10,7 +9,6 @@ import           Data.Bits
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Storable as VS
-import qualified Data.Vector.Unboxed as VU
 import           Control.Concurrent (setNumCapabilities)
 import           System.Console.GetOpt
 import           System.Environment (getArgs)
@@ -21,40 +19,12 @@ import           Data.Conduit.Algorithms.Utils
 import           Data.Conduit.Algorithms.Async
 
 import Control.Monad.IO.Class
-import System.IO.SafeWrite (withOutputFile)
-import StorableConduit (readWord32VS)
 import System.IO
 import Foreign.Storable
-import Foreign.Marshal.Alloc
-import Data.List
-import Control.Arrow
 import Control.Monad
 
-import StorableConduit (writeWord32VS)
-
-
+import Kmers
 import Data.BioConduit
-
-alphabetIndex :: VU.Vector Word32
-alphabetIndex = VU.fromList
-                    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  7,  0,  0, 13, 12,  1,  5, 11,  4,  0
-                    ,15,  3,  3, 10,  0,  6, 12, 14,  9,  8,  0,  4,  2,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-                    , 0,  0,  0,  0,  0,  0]
-
-encodeKMERS :: Int -> Fasta -> VS.Vector Word32
-encodeKMERS n (Fasta _ fa) = VS.fromList . concatMap (\k -> [k, toEnum n]) . drop 7 $ scanl k1 0 (B.unpack fa)
-    where
-        k1 :: Word32 -> Word8 -> Word32
-        k1 k b = let b' = alphabetIndex VU.! fromEnum b
-                        in ((k .&. 0xffffff) `shiftL` 4) .|. b'
 
 splitTopV :: VS.Vector Word32 -> [VS.Vector Word32]
 splitTopV v = [get k | k <- [0..15]]
@@ -85,7 +55,7 @@ parseArgs argv = foldl' (flip ($)) (CmdArgs "" "" False 1) flags
 
 
 writeToSplits base = do
-        hs <- liftIO $ sequence [openFile (base ++ "."++show i) WriteMode | i <- [0..15]]
+        hs <- liftIO $ sequence [openFile (base ++ "."++show i) WriteMode | i <- [0 :: Int ..15]]
         writeToSplits' hs
         forM_ hs (liftIO . hClose)
     where
---
+++ src/Kmers.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings, FlexibleContexts, BangPatterns, PackageImports #-}
+module Kmers
+    ( encodeKMERS
+    ) where
+
+import qualified Data.ByteString as B
+import           Data.Word
+import           Data.Bits
+
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Unboxed as VU
+
+import Data.BioConduit
+
+alphabetIndex :: VU.Vector Word32
+alphabetIndex = VU.fromList
+                    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  7,  0,  0, 13, 12,  1,  5, 11,  4,  0
+                    ,15,  3,  3, 10,  0,  6, 12, 14,  9,  8,  0,  4,  2,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+                    , 0,  0,  0,  0,  0,  0]
+
+encodeKMERS :: Int -> Fasta -> VS.Vector Word32
+encodeKMERS n (Fasta _ fa) = VS.fromList . concatMap (\k -> [k, toEnum n]) . drop 7 $ scanl k1 0 (B.unpack fa)
+    where
+        k1 :: Word32 -> Word8 -> Word32
+        k1 k b = let b' = alphabetIndex VU.! fromEnum b
+                        in ((k .&. 0xffffff) `shiftL` 4) .|. b'
